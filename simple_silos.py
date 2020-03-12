635b3658206fe45a457b0d06e4aff59dcb10325b
Matthew Cowan <matthew.lj.cowan@gmail.com>
2020-03-12 15:17:07 -0700

    deep6
    Project going on indefinite hold. This is the last commit.

--- simple_silos.py
+++ simple_silos.py
@@ -4,41 +4,38 @@ import pandas as pd
 
 def get_destination(unit,equipment_dict):
     """
-    Determine the destination and size of a grain packet
+    Determine the destination of the grain packet. Quantity to be transfered is
+    determined during the transfer function. This function only determines where
+    it should go.
     """
+    print(unit["name"])
     dest_str = None
     if unit["is_node"]:
         decision = unit["decision"]
         choice_ct = len(unit["value"])-1
-
         if unit[decision]<unit["value"][0]: #Lower than lowest threshold
+            print('node:lo')
             dest_str = unit["next"][0]
         elif unit[decision]>unit["value"][choice_ct]: #Higher than highest threshold
+            print('node:hi')
             dest_str = unit["next"][choice_ct+1]
         else: #Somewhere inbetween the min and max thresholds, check the indexing
+            print('node:var')
             for ct in range(choice_ct-1):
-                if unit[decision]>=unit["value"][ct] and unit[decision]>=unit["value"][ct+1]:
+                if unit[decision]>=unit["value"][ct] and unit[decision]<=unit["value"][ct+1]:
+                    print('node:'+str(ct+1))
                     dest_str = unit["next"][ct+1]
+    elif unit["is_sink"]:
+        dest_str = None
     else:
         dest_str = unit["next"]
 
     if dest_str is not None:
         destination = equipment_dict[dest_str]
     else:
-        destination = None
-        quantity = None
-        return (destination, quantity)
+        return None
 
-    if unit["contents"]==0: # I am empty
-        quantity = None
-    elif unit["contents"]<unit["transfer_rate"]: #I'm nearly empty
-        quantity = unit["contents"]
-    # elif unit["transfer_rate"]>(destination["max_contents"] - destination["contents"]): #Is my destination nearly full?
-    #     quantity = destination["max_contents"] - destination["contents"]
-    else: #Otherwise do a normal transfer
-        quantity = unit["transfer_rate"]
-
-    return (destination['name'], quantity)
+    return destination['name']
 
 def do_transfers(ledger, equipment_dict, cycle_max=3):
     """
@@ -50,34 +47,73 @@ def do_transfers(ledger, equipment_dict, cycle_max=3):
         for idx,entry in enumerate(ledger):
             origin = equipment_dict[entry[0]]
             destination = equipment_dict[entry[1]]
-            quantity = entry[2]
+            good_transfer = False
 
-            print(entry)
+            if origin["contents"] <= 0:
+                """
+                Origin is empty
+                """
+                print('dt:orig empty')
+                quantity = 0
+                good_transfer = False
+                ledger.pop(idx)
+            elif (destination["max_contents"]-destination["contents"]) <= 0:
+                """
+                Destination is full.
+                """
+                print('dt:dest full')
+                good_transfer = False
+            elif (destination["max_contents"]-destination["contents"]) < source["transfer_rate"]:
+                """
+                Destination is nearly full, fill up the destination
+                """
+                print("dt:dest near full")
+                quantity = destination["max_contents"]-destination["contents"]
+                good_transfer = True
+            elif origin["contents"] < source["transfer_rate"]:
+                """
+                Origin is nearly empty, take the last bit of grain
+                """
+                print('dt:orig near empty')
+                quantity = origin["contents"]
+                good_transfer = True
+            else:
+                """
+                Normal transfer, quantity is fine
+                """
+                print('dt:norm')
+                quantity = source["transfer_rate"]
+                good_transfer = True
 
-            if (destination["contents"]+quantity)<=destination["max_contents"]: #Destination is not nearly full
+            if good_transfer:
                 """
-                Note that the origin moisture content doesn't change when stuff is
-                removed. All grain packets are assumed to be homogenous.
+                This is a valid transfer so do all the math. Otherwise do
+                nothing and wait for the next pass through.
                 """
+                print("dt:transfer")
+                print("dt:"+str(quantity))
                 total_mass = destination["contents"]+quantity
                 transfer_water = origin["moisture"] * quantity
                 destination_water = destination["moisture"] * destination["contents"]
-
+                new_moisture = (destination_water + transfer_water)/total_mass
                 destination["contents"] = total_mass
-                destination["moisture"] = (destination_water + transfer_water)/total_mass
                 origin["contents"] = origin["contents"] - quantity
-
-                if origin["contents"] is 0:
-                    origin["moisture"] = 0
-
-                """
-                Traffic jam breaker. Try all the transfers once, pop the ones that
-                work and then go back and try the ones that didn't again.
-                """
+                if destination["is_drier"]:
+                    """
+                    Driers dry to their default dryness
+                    """
+                    destination["moisture"] = destination["moisture"]
+                else:
+                    """
+                    Everything else is mass averaged
+                    """
+                    destination["moisture"] = new_moisture
                 ledger.pop(idx)
-            elif (origin["contents"]-quantity)<0: #Origin is nearly empty, do nothing this time through
-                ledger.pop(idx)
+        print("***")
 
+    """
+    A handy little snippet to indicate whether all the transfers suceeded or not.
+    """
     if len(ledger) is 0:
         return True
     else:
@@ -146,7 +182,7 @@ delivery_schedule = model["delivery_schedule"]
 """
 Build data collectors
 """
-data_dict = {"time":[]}
+data_dict = {"time":[],"ledger_start":[],"ledger_end":[]}
 for unit in equipment:
     entry_name = unit["name"]+"_contents"
     data_dict.update({entry_name:[]})
@@ -159,17 +195,6 @@ Iterate through the equipment chain, do the transfers
 """
 while (time <= time_max):
 
-    """
-    Build the data table and append
-    Do all this at the start of the time frame
-    """
-    data_dict["time"] = time
-    for unit in equipment:
-        data_dict[unit["name"]+"_contents"] = unit["contents"]
-        data_dict[unit["name"]+"_moisture"] = unit["moisture"]
-    temp = pd.DataFrame(data_dict, index=[0])
-    data = data.append(temp)
-
     ledger = []
 
     """
@@ -177,19 +202,32 @@ while (time <= time_max):
     """
     for unit in equipment:
         origin = unit['name']
-        destination, quantity = get_destination(unit, equipment_dict)
-        entry = (origin,destination,quantity)
-        if destination is None or quantity is None:
+        destination = get_destination(unit, equipment_dict)
+        entry = (origin,destination)
+        if destination is None:
             pass
         else:
             ledger.append(entry)
-
-    #Execute transfers, calculate new moistures.
-    do_transfers(ledger, equipment_dict)
+    print("<"+str(time)+">"+str(ledger))
+    data_dict["ledger_start"] = str(ledger)
+    #Execute transfers and deliveries
+    truth = do_transfers(ledger, equipment_dict)
     do_delivery(source, time, delivery_schedule)
-
+    print(truth)
     time = time + 1 #Iterate time
     print("<<##>>")
+
+    """
+    Build the data table and append
+    Do all this at the start or end of the time frame?
+    """
+    data_dict["time"] = time
+    data_dict["ledger_end"] = str(ledger)
+    for unit in equipment:
+        data_dict[unit["name"]+"_contents"] = unit["contents"]
+        data_dict[unit["name"]+"_moisture"] = unit["moisture"]
+    temp = pd.DataFrame(data_dict, index=[0])
+    data = data.append(temp)
 
 """
 Do something with the data, like export to Excel
